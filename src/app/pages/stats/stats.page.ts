9140244cef0c114ba86ccba04f0f9df56b33a7bb
Patryk Bielecki <patryk.bielecki@nexpertis.pl>
2022-11-24 22:17:48 +0100

    stats page data visualisation

--- src/app/pages/stats/stats.page.ts
+++ src/app/pages/stats/stats.page.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { InfiniteScrollCustomEvent, LoadingController } from '@ionic/angular';
+import { UsersService } from 'src/app/services/users.service';
+import { environment } from 'src/environments/environment';
+import { ApiResult } from 'src/app/services/users.service';
 
 @Component({
-  selector: 'app-stats',
+  selector: 'app-users',
   templateUrl: './stats.page.html',
   styleUrls: ['./stats.page.scss'],
 })
 export class StatsPage implements OnInit {
+  users = []
 
-  constructor() { }
+  constructor(
+    private usersService: UsersService,
+    private loadingCtrl: LoadingController
+  ) {}
 
   ngOnInit() {
+    this.loadUsers();
   }
 
+  async loadUsers(event?: InfiniteScrollCustomEvent) {
+      const loading = await this.loadingCtrl.create({
+        message: 'Loading..',
+        spinner: 'bubbles',
+      });
+      await loading.present();
+
+      this.usersService.getAllUsers().subscribe(
+        (res) => {
+          loading.dismiss();
+          for(let i=0; i<res.length; i++){
+            this.users.push(res[i]);
+          }
+        },
+        (err) => {
+          console.log(err);
+          loading.dismiss();
+        }
+      );
+    }
 }
---
+++ src/app/services/users.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+export interface ApiResult {
+results: any[];
+total_pages: number;
+total_results: number;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  constructor(private http: HttpClient) { }
+
+  getAllUsers(): Observable<Array<Object>> {
+     return this.http.get<Array<Object>>(
+       `http://localhost:8080/api/all_users`
+     );
+  }
+}
